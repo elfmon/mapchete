4034f7b5c6fc09c202e3346c5d970b9c23583aa5
Joachim Ungar <joachim.ungar@eox.at>
2019-05-17 15:19:47 +0200

    dev commit

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ setup(
         ],
         'mapchete.formats.drivers': [
             'geojson=mapchete.formats.default.geojson',
+            'gpkg=mapchete.formats.default.gpgk',
             'gtiff=mapchete.formats.default.gtiff',
             'mapchete_input=mapchete.formats.default.mapchete_input',
             'png_hillshade=mapchete.formats.default.png_hillshade',
